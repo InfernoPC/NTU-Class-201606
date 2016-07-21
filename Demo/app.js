5b505a2fb3d8754a0188efc89d4fb8015b900425
Tim Chen <inferno6562@gmail.com>
2016-07-22 00:52:33 +0800

    final demo

---
+++ Demo/app.js
@@ -0,0 +1,11 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+  $(".pokemon-ball").click(function(){
+    $(this).css("animation", "throwball 1s ease");
+    
+  });
+  
+  
+});s
